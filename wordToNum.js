ad04222465c339463fc6e014a063d23fea56621e
mikeserato <mikeserato@gmail.com>
2016-02-12 10:48:53 +0800

    Initial Progress

---
+++ wordToNum.js
@@ -0,0 +1,23 @@
+var wordLibrary = {
+    'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'ten': 10,
+    'eleven': 11, 'twelve': 12, 'thirteen': 13, 'fourteen': 14, 'fifteen': 15,
+    'sixteen': 16, 'seventeen': 17, 'eighteen': 18, 'nineteen': 19, 'twenty': 20,
+    'thirty': 30, 'forty': 40, 'fifty': 50, 'sixty': 60, 'seventy': 70, 'eighty': 80, 'ninety': 90
+};
+
+function wordToNum(){
+	var word = document.getElementById("input").value;
+	console.log(word);
+
+	var n = word.split(' ');
+	console.log(n);
+
+	foreach(n) {
+		console.log(n);
+	}
+}
+
+function showResult(a) {
+	alert(a);
+}
